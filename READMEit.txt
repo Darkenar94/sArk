b593983b560d5755879840ab5c3a2c2f6a6d4d31
Darkenar94 <57009875+Darkenar94@users.noreply.github.com>
2020-04-02 15:52:57 +0200

    prima versione in italiano

---
+++ READMEit.txt
@@ -0,0 +1,54 @@
+
+********************************************************************************
+COSA FA QUESTO PROGRAMMA:
+********************************************************************************
+Questo programma è stato creato nell'intento di offrire una migliore gestione
+dei files di salvataggio e backup del video game ark survival evolved, poichè
+questo crea files di backup del salvataggio in utilizzo ma non offre la 
+possibilità, nel caso in cui l'utente ne avesse la necessità, di utilizzare un 
+precedente salvataggio mentre si è in gioco, ed è proprio qui che potrebbe 
+tornare utile sArkV16 risparmiando all'utente il tempo e la fatica di:
+dover sostituire il file di salvataggio in utilizzo con uno dei backups 
+creati automaticamente dal gioco; dover fare una copia dell'intera cartella 
+dedicata ai salvataggi e poi sostuirla con quest'ultima o semplicemente di 
+dover creare una copia di scorta dei files per motivi personali.  
+********************************************************************************
+UTILIZZO DEL PROGRAMMA:
+********************************************************************************
+Appena verrà avviato il programma chiederà all'utente con quale mappa si avrà 
+a che fare e la scelta verrà effettuata digitando il numero che
+corrisponde alla mappa da utilizzare, dopodichè controllerà in quale disco 
+locale e directory il salvataggio e i backups sono stati creati dal gioco. A
+questo punto è possibile scegliere fra tre opzioni: la prima, creerà un backup 
+completo quindi andrà a creare una cartella sul tuo desktop 'arkBackups' e al 
+suo interno una cartella con il nome della mappa che hai scelto dove copierà 
+tutti i files contenuti nella cartella dei salvataggi di ark survival evolved 
+riguardanti la mappa scelta; la seconda, sostituirà il salvataggio con il backup 
+creato dall'utente riguardante la mappa scelta in precedenza e la terza, offrirà 
+la possibilità di sostituire il salvataggio con uno dei backups creati in 
+automatico dal gioco. 
+
+# la seconda opzione inoltre prima di sostituire il salvataggio con il backup
+# creerà una cartella salvavita chiamata 'vecchioSalvataggio', contenente il
+# salvataggio che sta per essere sostituito dal backup.
+
+nel caso (raro) in cui non venissero localizzati i files di salvataggio 
+creati dal gioco il programma chiederà all'utente il percorso
+corretto in cui si trovano, andrà a creare la cartella 'arkBackups' sul tuo
+desktop e un file all'intero 'sArk.config' memorizzando il percorso contente
+salvataggi e backups, in modo che al prossimo avvio del programma non richieda
+più il percorso corretto.
+
+# non rinominare o spostare la cartella 'arkBackups' e il suo contenuto.
+
+********************************************************************************
+ATTENZIONE:
+********************************************************************************
+Programma attualmente funzionante per le seguenti mappe: 
+TheIsland; TheCenter; Ragnarok e Valguero.
+********************************************************************************
+TIPS AND TRICKS:
+********************************************************************************
+1. cheat saveworld > sArkV16 > backup completo 
+2. problema in-game? > sostituzione salvataggio con backup > riavvio ark
+*******************************************************************************
---
+++ sArkV1.py
@@ -0,0 +1,287 @@
+
+# COMPILARE IL PROGRAMMA:
+# cd C:\Users\Darkenar94\AppData\Local\Programs\Python\Python38-32\scripts
+# pyinstaller percorso\File.py
+
+# AGGIUNGERE L'ICONA.
+
+# stampare errori su un file ???
+# aggiornamento del programma in caso di nuova versione ???
+
+import os
+import time
+from datetime import datetime
+
+def separa():
+    print("----------------------------------------------------------------------------------------------------------")
+
+def cartella(salvataggi, mappa):
+    cartella = os.path.split(salvataggi)
+    salvataggi = cartella[0] + mappa + cartella[1]
+    return salvataggi
+
+def creaConfigFile(percorso, cartella):
+    file = open(os.path.join(percorso, "sArk.config"), "a")
+    file.write(cartella + "\n")
+    file.close()
+
+def errore(cartella):
+    cartellaBackup = os.getenv("USERPROFILE") + "\\Desktop\\arkBackups"
+    while os.path.exists(cartella) == False or "SavedArksLocal" not in cartella or len(os.listdir(cartella)) == 0:
+        separa()
+        print("errore: cartella contenente salvataggi ARK non trovata o eventualmente VUOTA. ")
+        print("continuare a giocare perchè il gioco crei automaticamente i salvataggi o")
+        print("inserire manualmente il percorso corretto...")
+        separa()
+        cartella = os.path.abspath(input("> "))
+    if os.path.exists(cartellaBackup):
+        pass
+    else:
+        os.mkdir(cartellaBackup)
+    creaConfigFile(cartellaBackup, cartella)
+    return cartella
+
+def dividi(percorsi):
+    sep = "\n"
+    return percorsi.split(sep)
+
+def cerca(tmpConfigFile, mappa):
+    configFile = dividi(tmpConfigFile)
+    for percorso in configFile:
+        cartella = os.path.split(percorso)
+        if mappa + "SavedArksLocal" == cartella[1]:
+            if os.path.exists(percorso):
+                configFile = percorso
+                return configFile
+    for percorso in configFile:
+        cartella = os.path.split(percorso)
+        if cartella[1] == "SavedArksLocal":
+            if os.path.exists(percorso):
+                configFile = percorso
+                return configFile
+    os.remove(os.getenv("USERPROFILE") + "\\Desktop\\arkBackups\\sArk.config")
+    errore(percorso)
+
+def leggiFile(percorso, mappa):
+    file = open(os.path.join(percorso, "sArk.config"), "r")
+    tmpConfigFile = file.read()
+    file.close()
+    configFile = cerca(tmpConfigFile, mappa)
+    return configFile
+
+def definisciPercorso(alfabeto, salvataggi):
+    for lettera in alfabeto:
+        percorso = lettera + ":\\Program Files (x86)" + salvataggi[2:]
+        if os.path.exists(percorso):
+            salvataggi = percorso
+            return salvataggi
+        percorso = lettera + salvataggi[1:]
+        if os.path.exists(percorso):
+            salvataggi = percorso
+            return percorso
+    salvataggi = errore(percorso)
+    return salvataggi
+
+def esiste(salvataggi, alfabeto):
+    if os.path.exists(salvataggi):
+        return salvataggi
+    else:
+        salvataggi = definisciPercorso(alfabeto,salvataggi)
+        return salvataggi
+
+def svuotare(cartella):
+    for file in os.listdir(cartella):
+        if file == "sArk.config":
+            pass
+        else:
+            os.remove(os.path.join(cartella,file))
+
+def stampaBackups(salvataggi, lista, num):
+    for file in os.listdir(salvataggi):
+            if "_" in file[11:]:
+                percorsoFile = os.path.join(salvataggi, file)
+                fileStat = os.stat(percorsoFile)
+                fileInfo = datetime.fromtimestamp(fileStat.st_mtime)
+                print(str(num) + ".", file, "{}/{}/{} {}:{}".format(fileInfo.day, fileInfo.month, fileInfo.year, fileInfo.hour, fileInfo.minute))
+                lista.append(percorsoFile)
+                num += 1
+    if len(lista) == 0:
+        return None
+    else:
+        print(str(num) + ". annullare l'operazione")
+
+
+cartellaBackup = os.getenv("USERPROFILE") + "\\Desktop\\arkBackups"
+salvataggi = "D:\\Steam\\steamapps\\common\\ARK\\ShooterGame\\Saved\\SavedArksLocal"
+mappa = "x"
+
+print("\n------------------------------------------------------------------------- sArkV1 By Darkenar94 -----------")
+print("Con quale mappa si desidera lavorare:")
+separa()
+print("1. TheIsland\n2. TheCenter\n3. Ragnarok\n4. Valguero")
+separa()
+
+while True:
+    operazione = input(">>> ")
+    separa()
+    if operazione == "1":
+        print("mappa selezionata. " + "(TheIsland)")
+        mappa = "TheIsland"
+        break
+    elif operazione == "2":
+        salvataggi = cartella(salvataggi, "\\TheCenter")
+        print("mappa selezionata. " + "(TheCenter)")
+        mappa = "TheCenter"
+        break
+    elif operazione == "3":
+        salvataggi = cartella(salvataggi, "\\Ragnarok")
+        print("mappa selezionata. " + "(Ragnarok)")
+        mappa = "Ragnarok"
+        break
+    elif operazione == "4":
+        salvataggi = cartella(salvataggi, "\\Valguero")
+        print("mappa selezionata. " + "(Valguero)")
+        mappa = "Valguero"
+        break
+    else:
+        print("opzione inesistente.")
+        separa()
+
+cartellaMappa = os.path.join(cartellaBackup, mappa)
+
+alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+if os.path.exists(os.path.join(cartellaBackup, "sArk.config")):
+    salvataggi = leggiFile(cartellaBackup, mappa)
+else:
+    salvataggi = esiste(salvataggi, alfabeto)
+
+lista = []
+num = 1
+
+separa()
+print("scegliere l'operazione da eseguire:")
+separa()
+print("1. eseguire un backup completo.\n2. sostituire backup completo > salvataggio.\n3. utilizzare backup automatici di ark.\n4. chiudere programma.")
+separa()
+
+while True:
+    opzione = input(">> ")
+    separa()
+    if opzione == "1":
+        if os.path.exists(cartellaBackup):
+            pass
+        else:
+            os.mkdir(cartellaBackup)
+        if esiste(salvataggi, alfabeto):
+            pass
+        print("operazione creazione backup avviata.")
+        separa()
+        try:
+            os.mkdir(cartellaMappa)
+        except:
+            print("backup già esistente, creazione nuovo backup in corso...")
+            separa()
+            svuotare(cartellaMappa)
+        os.system("copy " + '"' + salvataggi + '"' + " " + '"' + cartellaMappa + '"')
+        separa()
+        print("backup effettuato.")
+        separa()
+    elif opzione == "2":
+        print("ATTENZIONE: l'opzione scelta farà si che il file di salvataggio venga sostituito con il backup.")
+        separa()
+        scelta = input("Procedere? S/n: ")
+        scelta = scelta.lower()
+        if scelta == "s":
+            if esiste(salvataggi, alfabeto):
+                pass
+            separa()
+            listaFiles = os.listdir(os.getenv("USERPROFILE") + "\\Desktop")
+            if "arkBackups" in listaFiles and len(os.listdir(cartellaBackup)) > 0:
+                print("operazione sostituzione files avviata.")
+                percorsoDiviso = os.path.split(salvataggi)
+                vecchioSalvataggio = percorsoDiviso[0] + "\\vecchioSalvataggio" + mappa
+                try:
+                    separa()
+                    print("creazione salvavita in corso...")
+                    os.mkdir(vecchioSalvataggio)
+                except:
+                    separa()
+                    print("salvavita già presente. creazione nuovo salvavita in corso...")
+                    svuotare(vecchioSalvataggio)
+                separa()
+                if len(os.listdir(salvataggi)) > 0:
+                    os.system("copy " + '"' + salvataggi + '"' + " " + '"' + vecchioSalvataggio + '"')
+                    separa()
+                    print("salvavita creato in:", percorsoDiviso[0])
+                else:
+                    print("nessun salvataggio trovato impossibile trasferire files in cartella salvavita.")
+                separa()
+                print("sostituzione files in corso...")
+                separa()
+                svuotare(salvataggi)
+                os.system("copy " + '"' + cartellaMappa + '"' + " " + '"' + salvataggi + '"')
+                separa()
+                print("sostituzione files affettuata con successo.")
+                separa()
+            else:
+                print("impossibile eseguire l'operazione. Backup non trovato.")
+                print("creare nuovo backup o chiudere il programma.")
+                separa()
+                continue
+        elif scelta == "n":
+            separa()
+            print("operazione annullata.")
+            separa()
+        else:
+            separa()
+            print("comando non riconosciuto.")
+            separa()
+    elif opzione == "3":
+        if esiste(salvataggi, alfabeto):
+            pass
+        print("backup creati dal gioco:")
+        separa()
+        stampaBackups(salvataggi, lista, num)
+        if len(lista) == 0:
+            print("nessun backup creato automaticamente dal gioco.")
+            separa()
+            continue
+        separa()
+        print("selezionare il backup che sostituirà il salvataggio.")
+        separa()
+        while True:
+            opzione = input("> ")
+            separa()
+            try:
+                opzione = int(opzione)
+            except ValueError:
+                print("digitare un numero.")
+                separa()
+            else:    
+                if opzione > 0 and opzione <= len(lista):
+                    file = mappa + ".ark"
+                    os.rename(os.path.join(salvataggi, file), os.path.join(salvataggi, "delete"))
+                    os.remove(os.path.join(salvataggi, "delete"))
+                    os.rename(lista[opzione -1], os.path.join(salvataggi, file))
+                    print("sostituzione files affettuata con successo.")
+                    lista = []
+                    separa()
+                    break
+                elif opzione == len(lista) +1:
+                    print("operazione annullata.")
+                    lista = []
+                    separa()
+                    break
+                else:
+                    print("opzione inesistente.")
+                    separa()
+    elif opzione == "4":
+        print("richiesta arresto programma. arresto automatico tra (3) secondi...")
+        separa()
+        time.sleep(3)
+        quit()
+    else:
+        print("opzione inesistente.")
+        separa()
+
